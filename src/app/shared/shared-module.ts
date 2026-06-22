14b7088053088f6ca8b6d21803edc263af3859ba
VictorPedroza <victor.pedroza@protonmail.com>
2026-06-22 18:48:50 -0300

    ✨ feat:(home): Add home page on shared module

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/common/button-component/button-component';
 import { HeaderComponent } from './components/layouts/header-component/header-component';
+import { HomePage } from './pages/home-page/home-page';
+import { AppRoutingModule } from "../app-routing-module";
 
 @NgModule({
-  declarations: [ButtonComponent, HeaderComponent],
-  imports: [CommonModule],
+  declarations: [ButtonComponent, HeaderComponent, HomePage],
+  imports: [CommonModule, AppRoutingModule],
   exports: [HeaderComponent],
 })
 export class SharedModule {}
