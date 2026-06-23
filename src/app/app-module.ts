c311244e6b0fa53361aa6f36a77e5d20433e2520
pearwaz1n <pedrolocatelli466@gmail.com>
2026-06-23 14:17:30 -0300

    feat(auth): implementation of Imports, declarations and routes

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,18 +1,19 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { SharedModule } from './shared/shared-module';
-import { CoreModule } from './core/core-module';
 import { AuthModule } from './modules/auth/auth-module';
 import { InviteModule } from './modules/invite/invite-module';
 import { MarketplaceModule } from './modules/marketplace/marketplace-module';
+import { CoreModule } from './core/core-module';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
+
 
 @NgModule({
-  declarations: [
-    App
-  ],
+  declarations: [App],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -20,11 +21,12 @@ import { MarketplaceModule } from './modules/marketplace/marketplace-module';
     SharedModule,
     AuthModule,
     InviteModule,
-    MarketplaceModule
+    MarketplaceModule,
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideHttpClient(withInterceptors([authInterceptor]))
   ],
-  bootstrap: [App]
+  bootstrap: [App],
 })
 export class AppModule { }
