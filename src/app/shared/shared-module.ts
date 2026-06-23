6311348324b78c3a1b8b6e9c53943d9d7335d236
pearwaz1n <pedrolocatelli466@gmail.com>
2026-06-23 11:11:01 -0300

    feat(shared): integrating imports of new components and footer  on main layout

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/common/button-component/button-component';
 import { HeaderComponent } from './components/layouts/header-component/header-component';
 import { HomePage } from './pages/home-page/home-page';
-import { AppRoutingModule } from "../app-routing-module";
+import { AppRoutingModule } from '../app-routing-module';
+import { FooterComponent } from './components/layouts/footer-component/footer-component';
+import { CronogramaComponent } from './components/home-page/cronograma-component/cronograma-component';
+import { HistoriaComponent } from './components/home-page/historia-component/historia-component';
 
 @NgModule({
-  declarations: [ButtonComponent, HeaderComponent, HomePage],
+  declarations: [
+    ButtonComponent,
+    HeaderComponent,
+    HomePage,
+    FooterComponent,
+    CronogramaComponent,
+    HistoriaComponent,
+  ],
   imports: [CommonModule, AppRoutingModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, FooterComponent, CronogramaComponent, HistoriaComponent],
 })
-export class SharedModule {}
+export class SharedModule { }
