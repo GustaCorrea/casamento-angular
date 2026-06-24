7a68a1187297a7baf085627157bd7bef3812646b
pearwaz1n <pedrolocatelli466@gmail.com>
2026-06-24 10:54:29 -0300

    feat(adminGuests): add admin guests service

---
+++ src/app/modules/admin/services/admin-guests-service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ApiService } from '../../../core/services/api-service';
+
+export interface Convidado {
+  id?: number;
+  nome: string;
+  email: string;
+  telefone: string;
+  restricoes: string;
+  limiteAcompanhantes: number;
+  status: 'Confirmado' | 'Aguardando' | 'Recusado';
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdmGuestsService {
+
+  constructor(private api: ApiService) { }
+  getConvidados(): Observable<Convidado[]> {
+    return this.api.get<Convidado[]>("/admin/convidados");
+  }
+
+  addConvidado(convidado: Convidado): Observable<Convidado> {
+    return this.api.post<Convidado>("/admin/convidados", convidado);
+  }
+
+  updateConvidado(id: number, convidado: Convidado): Observable<Convidado> {
+    return this.api.put<Convidado>(`/admin/convidados/${id}`, convidado);
+  }
+
+  deleteConvidado(id: number): Observable<void> {
+    return this.api.delete<void>(`/admin/convidados/${id}`);
+  }
+}
