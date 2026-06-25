b67133edad1112ecf3a693fe906cb44b752aa914
Gustavo correa <gustavocorrea.1910@gmail.com>
2026-06-25 13:30:20 -0300

    chore: import FormsModule in admin module for data binding

--- src/app/modules/admin/admin-module.ts
+++ src/app/modules/admin/admin-module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PresentesPage } from './pages/presentes-page/presentes-page';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PresentesPage],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
 })
 export class AdminModule {}
