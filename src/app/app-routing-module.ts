232d964eee2f168699b07852974d78eec9088dff
VictorPedroza <victor.pedroza@protonmail.com>
2026-06-25 00:23:21 -0300

    🐛 fix:(routes): Fix layouts to use invite layout on invite page

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -6,6 +6,7 @@ import { AuthLayout } from './core/layouts/auth-layout/auth-layout';
 import { LoginPage } from './modules/auth/pages/login-page/login-page';
 import { AdminLayout } from './core/layouts/admin-layout/admin-layout';
 import { InvitePage } from './modules/invite/pages/invite-page/invite-page';
+import { InviteLayout } from './core/layouts/invite-layout/invite-layout';
 
 const routes: Routes = [
   {
@@ -16,9 +17,11 @@ const routes: Routes = [
   },
   {
     // Layout de Convites
-    path: 'invite',
-    component: MainLayout,
-    children: [{ path: '', component: InvitePage }],
+    path: '',
+    component: InviteLayout,
+    children: [
+      { path: 'invite', component: InvitePage }
+    ],
   },
   {
     // Layout da Autenticação
