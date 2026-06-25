24e24998828bacfbe8f3c18baafa4cb6e95d1c4f
VictorPedroza <victor.pedroza@protonmail.com>
2026-06-24 23:16:34 -0300

    🐛 fix:(modal): Remove marketplace/components on root dir

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -11,10 +11,9 @@ import { MarketplaceModule } from './modules/marketplace/marketplace-module';
 import { CoreModule } from './core/core-module';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { AdminModule } from './modules/admin/admin-module';
-import { Modal } from './marktplace/components/modal/modal';
 
 @NgModule({
-  declarations: [App, Modal],
+  declarations: [App],
   imports: [
     BrowserModule,
     AppRoutingModule,
