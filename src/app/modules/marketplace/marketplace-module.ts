64b550242bb1d15e2226a4bf315dcbac8cf278f3
VictorPedroza <victor.pedroza@protonmail.com>
2026-06-24 23:07:46 -0300

    📦 build:(modal): Add form module on Marketplace Module

--- src/app/modules/marketplace/marketplace-module.ts
+++ src/app/modules/marketplace/marketplace-module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { PrizePage } from './pages/prize-page/prize-page';
 import { CardComponent } from './components/card-component/card-component';
 import { Modal } from './components/modal/modal';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   declarations: [CardComponent, PrizePage, Modal],
   exports: [PrizePage],
 })
