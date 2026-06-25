27f1ad2fa3a0275bc4f746b780c9936faf2731be
Gustavo correa <gustavocorrea.1910@gmail.com>
2026-06-25 18:48:17 -0300

    feat(sidebar): atualiza navegacao, hover e indicadores ativos do painel

--- src/app/shared/components/layouts/sidebar-component/sidebar-component.ts
+++ src/app/shared/components/layouts/sidebar-component/sidebar-component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sidebar-component',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar-component.css',
   host: { style: 'display: contents' }
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+
+  constructor(private router: Router) {}
+
+  logout() {
+    // Aqui no futuro limpar o token do usuário (ex: localStorage.clear())
+    this.router.navigate(['/auth/login']); 
+  }
+}
