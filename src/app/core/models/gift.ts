b0a6fb2e0ecc6b7be01b667409afbfedba452739
Gustavo correa <gustavocorrea.1910@gmail.com>
2026-06-25 12:03:18 -0300

    feat: create gift data model interface

---
+++ src/app/core/models/gift.ts
@@ -0,0 +1,11 @@
+export interface Gift {
+  id?: number;
+  name: string;
+  description: string;
+  totalValue: number;
+  imageUrl?: string;
+  category: string;
+  
+  collected: number;
+  status: 'ATIVO' | 'COMPLETO' | 'INDISPONIVEL'; 
+}
